a25fc301cc3a539544b2b77db1dea6a583b86899
laon2019 <laon5987@nate.com>
2022-12-25 21:30:56 +0900

    web

--- js/script_main.js
+++ js/script_main.js
@@ -26,3 +26,29 @@ $(function(){
         once : true //스크롤시 딱 한번만 하고 싶을 땐 true ||  반복 : path
     });
 });
+
+//배경색 변경
+$(window).on('scroll resize', function(){ /* window를 scroll 할때와 사이즈를 변경할때*/
+    var scrollTop = $(document).scrollTop(); /*변수 scrollTop에는 웹페이지 수직의 위치값 변수 scrollTop에 저장*/
+    bgColor();
+    function bgColor(){
+        if(scrollTop>1400){   /*1400이상이거나 2700보다 작은경우 on class 활성화 */
+            $('body').addClass('on');
+        }else{
+            $('body').removeClass('on');
+        }
+        if(scrollTop > 2700){
+            $('body').removeClass('on');
+        }
+    }
+});
+
+// 햄버거 메뉴
+$(function(){
+    $('.menuOpen button.open').on('click', function(){
+        $('.menuOpen .menuWrap').addClass('on');
+    });
+    $('.menuOpen .menuWrap .close').on('click', function(){
+        $('.menuOpen .menuWrap').removeClass('on');
+    });
+});
